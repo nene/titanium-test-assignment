8b36567840cbe7c3d12987bbf516403f2ffdd4cb
Rene Saarsoo <nene@triin.net>
2013-12-02 14:27:03 +0200

    Add MySQL database schema.

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+-- Table structure for "car prices" database.
+
+CREATE TABLE countries (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE
+) ENGINE = INNODB, CHARACTER SET = utf8, COLLATE = utf8_general_ci;
+
+CREATE TABLE cities (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    country_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+) ENGINE = INNODB, CHARACTER SET = utf8, COLLATE = utf8_general_ci;
+
+CREATE TABLE car_types (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE
+) ENGINE = INNODB, CHARACTER SET = utf8, COLLATE = utf8_general_ci;
+
+CREATE TABLE car_prices (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    city_id INT NOT NULL,
+    car_type_id INT NOT NULL,
+    price DECIMAL(15,2) NOT NULL,
+    FOREIGN KEY (city_id) REFERENCES cities(id),
+    FOREIGN KEY (car_type_id) REFERENCES car_types(id)
+) ENGINE = INNODB, CHARACTER SET = utf8, COLLATE = utf8_general_ci;
